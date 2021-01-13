666e417fddf2e37213eaeb6af77e5582632cfdae
jyhan732 <jyhan@yahoo.com>
2021-01-12 19:29:02 -0500

    mongodb connection changes to use env variable MONGODB_URI

--- server.js
+++ server.js
@@ -6,17 +6,22 @@ const PORT = 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
